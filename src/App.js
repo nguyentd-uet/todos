bccc0175d27311632520e02eeeb222a97bad4b1d
nguyentd1501 <nguyen.td1501@gmail.com>
2017-11-28 23:13:43 +0700

    update App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoList from './components/TodoList';
+import AddTodoForm from './components/AddTodoForm';
+import Filter from './components/Filter';
+import { Container, Col, Row } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Container>
+          <Row>  
+            <Col sm={{ size: 6 }}>
+              <AddTodoForm />
+              <br/>
+              <Filter />
+            </Col>
+            <Col sm={{ size: 6 }}>
+              <TodoList />
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
